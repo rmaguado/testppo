0ac0a12adf48c186a07d034f7ed7c7fc0b4e77f3
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-12-09 16:16:43 +0000

    patch episode logs

--- ppo_eval.py
+++ ppo_eval.py
@@ -1,7 +1,5 @@
-from typing import Callable
-
-import gymnasium as gym
 import torch
+import numpy as np
 
 
 def evaluate(
@@ -18,17 +16,19 @@ def evaluate(
 
     obs, _ = envs.reset()
     episodic_returns = []
+    print("Starting evaluation")
     while len(episodic_returns) < eval_episodes:
         actions, _, _, _ = agent.get_action_and_value(torch.Tensor(obs).to(device))
         next_obs, _, _, _, infos = envs.step(actions.cpu().numpy())
-        if "final_info" in infos:
-            for info in infos["final_info"]:
-                if "episode" not in info:
-                    continue
-                print(
-                    f"eval_episode={len(episodic_returns)}, episodic_return={info['episode']['r']}"
-                )
-                episodic_returns += [info["episode"]["r"]]
+        if "episode" in infos.keys():
+            episode_info = infos["episode"]
+            ep_returns = episode_info["r"]
+            ep_completions = episode_info["_r"]
+            mean_returns = np.mean(ep_returns[ep_completions])
+            print(
+                f"eval_episode={len(episodic_returns)}, episodic_return={mean_returns}"
+            )
+            episodic_returns += [mean_returns]
         obs = next_obs
 
     return episodic_returns
